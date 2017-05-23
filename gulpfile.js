c04e5b865578b6e76c7d07e995c88b0cfc77139f
Ben <ba.lowbridge@gmail.com>
2017-05-23 12:10:35 +1000

    began profile page

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,11 @@ var gulp = require('gulp'),
     sass = require('gulp-sass');
 
     gulp.task('sass',function(){
-      gulp.src(['scss/styles.scss'])
-        .pipe(sass(
-          {outputStyle:'expanded'}
-        ))
+      gulp.src(['scss/style.scss'])
+        .pipe(sass())
         .pipe(gulp.dest('css'))
+    });
+
+    gulp.task('watch', function(){
+    	gulp.watch(['scss/**/*.scss'], ['sass']);
     });
