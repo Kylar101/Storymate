80de7df62496c5c2dc1e3b986ab6971b435a9ba0
Ben <ba.lowbridge@gmail.com>
2017-06-07 19:08:03 +1000

    updating webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -23,10 +23,7 @@ module.exports = {
 	        'sass-loader'
 	        ]
 	    })
-  	},/*{
-  		test: /\.jpg$/,
-  		use: ['ignore-loader']
-  	},*/{
+  	},{
         test: /\.js$/,
         exclude: [/node_modules/],
         use: [{
