33664bd004c0e43b8b257488edb5b8c4399bbec5
Ben <ba.lowbridge@gmail.com>
2017-09-03 21:11:35 +1000

    making search query more efficient

--- dist/search.php
+++ dist/search.php
@@ -62,19 +62,11 @@ $userRow = mysqli_fetch_array($userRes);
 					if(isset($_POST['search'])){
 
 						$term = mysqli_real_escape_string($conn, $_POST['search']);
-					 	$sql = "SELECT * FROM stories WHERE title LIKE '%$term%' OR  description LIKE '%$term%'  ORDER BY title";
+					 	$sql = "SELECT * FROM stories INNER JOIN users ON stories.authorID = users.userID WHERE title LIKE '%$term%' OR  description LIKE '%$term%'  ORDER BY title";
 					 	$searchQuery = mysqli_query($conn,$sql);
 					 	$numResults = mysqli_num_rows($searchQuery);
 						
 						while($stories = mysqli_fetch_array($searchQuery)){
-
-							$authorID = $stories['authorID'];
-							$authorSQL = "SELECT * FROM users WHERE userID = '$authorID'";
-							$authorQuery = mysqli_query($conn,$authorSQL);
-							if (!$authorQuery) {
-								die (mysql_error($conn));
-							}
-						 	$authorResults = mysqli_fetch_array($authorQuery);
 
 						 	$storyID = $stories['storyID'];
 
@@ -93,7 +85,7 @@ $userRow = mysqli_fetch_array($userRes);
 							<div class="story-info">
 								<h3 class="title"><?php echo $stories['title']; ?></h3>
 								<p class="excerpt"><?php echo $stories['description']; ?></p>
-								<h4 class="author-name"><?php echo $authorResults['firstName'].' '.$authorResults['lastName'] ?></h4>
+								<h4 class="author-name"><?php echo $stories['firstName'].' '.$stories['lastName'] ?></h4>
 								<div class="story-extra">
 									<div class="likes">27 <span class="feature-likes"> <i class="fa fa-thumbs-up" aria-hidden="true"></span></i></div>
 									<a href=view-story.php?storyID=<?php echo $stories['storyID'];?> class="btn comments-button view-story">View Story</a>
@@ -109,17 +101,9 @@ $userRow = mysqli_fetch_array($userRes);
 				?>
 					
 					<?php
-						$sql = "SELECT * FROM stories ORDER BY storyID Desc LIMIT 10";
+						$sql = "SELECT * FROM stories INNER JOIN users ON stories.authorID = users.userID ORDER BY storyID Desc LIMIT 10";
 						$topRes = mysqli_query($conn,$sql);
 						while($stories = mysqli_fetch_array($topRes)){
-
-							$authorID = $stories['authorID'];
-							$authorSQL = "SELECT * FROM users WHERE userID = '$authorID'";
-							$authorQuery = mysqli_query($conn,$authorSQL);
-							if (!$authorQuery) {
-								die (mysql_error($conn));
-							}
-						 	$authorResults = mysqli_fetch_array($authorQuery);
 
 						 	$storyID = $stories['storyID'];
 
@@ -137,7 +121,7 @@ $userRow = mysqli_fetch_array($userRes);
 							<div class="story-info">
 								<h3 class="title"><?php echo $stories['title']; ?></h3>
 								<p class="excerpt"><?php echo $stories['description']; ?></p>
-								<h4 class="author-name"><?php echo $authorResults['firstName'].' '.$authorResults['lastName'] ?></h4>
+								<h4 class="author-name"><?php echo $stories['firstName'].' '.$stories['lastName'] ?></h4>
 								<div class="story-extra">
 									<div class="likes">27 <span class="feature-likes"> <i class="fa fa-thumbs-up" aria-hidden="true"></span></i></div>
 									<a href="view-story.php?storyID=<?php echo $stories['storyID'];?>" class="btn comments-button view-story">View Story</a>
