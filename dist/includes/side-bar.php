d18c4f306202f0948a47e8acdb666ce69b469e4d
Ben <ba.lowbridge@gmail.com>
2017-10-03 19:16:42 +1000

    adding admin title to side bar

--- dist/includes/side-bar.php
+++ dist/includes/side-bar.php
@@ -7,25 +7,27 @@
 
 <nav id="side-bar">
 	<ul>
-		<li class="profile"><a href="profile.php"><i class="fa fa-list-ul" aria-hidden="true"></i> My Account</a></li>
+		<li class="profile"><a href="profile.php"><i class="fa fa-user" aria-hidden="true"></i> My Account</a></li>
 		<li class="post-story"><a href="post-story.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Post Story</a></li>
 		<li class="search"><a href="search.php"><i class="fa fa-search" aria-hidden="true"></i> Search Stories</a></li>
 		<li class="logout"><a href="./php/log_out.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout </a></li>
 		<?php if ($userRole == 1) : ?>
-		<li class="manage-stories"><a href="manage-stories.php"><i class="fa fa-list-ul" aria-hidden="true"></i> Manage Stories</a></li>
-		<li class="manage-categories"><a href="manage-categories.php"><i class="fa fa-list-ul" aria-hidden="true"></i> Manage Categories</a></li>
+		<li><h5 class="admin">Administrator</h5></li>
+		<li class="manage-stories"><a href="manage-stories.php"><i class="fa fa-book" aria-hidden="true"></i> Manage Stories</a></li>
+		<li class="manage-categories"><a href="manage-categories.php"><i class="fa fa-list-alt" aria-hidden="true"></i> Manage Categories</a></li>
 		<?php endif; ?>
 	</ul>
 </nav>
 <nav id="side-bar-mobile">
 	<ul>
-		<li class="profile"><a href="profile.php"><i class="fa fa-list-ul" aria-hidden="true"></i> My Account</a></li>
+		<li class="profile"><a href="profile.php"><i class="fa fa-user" aria-hidden="true"></i> My Account</a></li>
 		<li class="post-story"><a href="post-story.php"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Post Story</a></li>
 		<li class="search"><a href="search.php"><i class="fa fa-search" aria-hidden="true"></i> Search Stories</a></li>
 		<li class="logout"><a href="./php/log_out.php"><i class="fa fa-sign-out" aria-hidden="true"></i> Logout </a></li>
 		<?php if ($userRole == 1) : ?>
-		<li class="manage-stories"><a href="manage-stories.php"><i class="fa fa-list-ul" aria-hidden="true"></i> Manage Stories</a></li>
-		<li class="manage-categories"><a href="manage-categories.php"><i class="fa fa-list-ul" aria-hidden="true"></i> Manage Categories</a></li>
+		<li><h5 class="admin">Administrator</h5></li>
+		<li class="manage-stories"><a href="manage-stories.php"><i class="fa fa-book" aria-hidden="true"></i> Manage Stories</a></li>
+		<li class="manage-categories"><a href="manage-categories.php"><i class="fa fa-list-alt" aria-hidden="true"></i> Manage Categories</a></li>
 		<?php endif; ?>
 	</ul>
 </nav>
